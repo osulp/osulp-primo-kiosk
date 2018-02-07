5c79d0ecf5a3b24b8f39e6f5a2ad0edbabb616ec
Dan M <dan.moore@oregonstate.edu>
2018-02-07 10:49:33 -0800

    added LivNav

--- OSU_KIOSK/js/custom.js
+++ OSU_KIOSK/js/custom.js
@@ -4,6 +4,32 @@
 
 var app = angular.module('viewCustom', ['angularLoad']);
    
+  /*<!-- LibNav Widget Integration -->*/
+  app.controller('prmSearchResultAvailabilityLineAfterController',  ['angularLoad', function (angularLoad) {
+    var w = this;
+    var w = document.createElement("script");
+    w.type = "text/javascript";
+    w.async = true;
+    w.src = "http://navigation.library.oregonstate.edu/widget/lib-nav.js";
+    var b = document.getElementsByTagName("script")[0];
+    b.parentNode.insertBefore(w, b);
+
+    var css2 = document.createElement("link");
+    css2.type = "text/css";
+    css2.rel = "Stylesheet";
+    css2.href = "http://navigation.library.oregonstate.edu/widget/lib-nav.css";
+    var a = document.getElementsByTagName("script")[0];
+    a.parentNode.insertBefore(css2, a);
+  }]);
+
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { },
+    controller: 'prmSearchResultAvailabilityLineAfterController',
+    template: `<div class="libnav-map">
+    </div>`
+  });
+  /*<!-- LibNav End -->*/
+
 // show/hide borrowing institutions   
 app.component('prmAlmaMoreInstAfter', {
     controller: 'institutionToggleController',
